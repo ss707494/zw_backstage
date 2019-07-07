628e077598643602a22f6b27e3aa2c708f683cba
ss <ss707494@163.com>
2019-07-07 22:04:52 +0800

    feat: 切换真实接口

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,10 @@
 const proxy = require('http-proxy-middleware');
 
+// http://www.marketpayless.com//Products//QueryCommodityType?SortType=1&IsAsc=1&Page=1&FloatPageCount=10&ParentID
 module.exports = function(app) {
   app.use(proxy('/Api', {
-    target: 'http://rap2api.taobao.org/app/mock/222495',
+    // target: 'http://rap2api.taobao.org/app/mock/222495',
+    target: 'http://www.marketpayless.com',
     changeOrigin: true,
     pathRewrite: {
       '^/Api': ''
