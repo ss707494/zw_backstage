50d5b64c613227acd7da5c5ead856fcf83cd3893
shensheng <shensheng@31huiyi.com>
2019-07-16 11:28:27 +0800

    feat: env

---
+++ files/upload_file2.bat
@@ -0,0 +1,2 @@
+
+scp -r root@128.14.236.90:/usr/ss707494/git_clone/flutter-go ./
--- src/common/plugins.js
+++ src/common/plugins.js
@@ -1,10 +1,10 @@
-import { isDev, host } from "@/common/global";
+import { host } from "@/common/global";
 
 axios.defaults.timeout = 0
 axios.defaults.withCredentials = true
 axios.interceptors.request.use(
     async config => {
-      const preUrl = isDev ? '/Api' : ''
+      const preUrl = process.env.REACT_APP_PRE_API || ''
       config.headers['XRequested-With'] = 'XMLHttpRequest'
       config.headers['Access-Control-Allow-Origin'] = '*'
       // config.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMyIsImlhdCI6MTU1MDAzODQyMCwiZXhwIjoxNTUwMjExMjIwfQ.4425yGKLu9zhEvMTatG-6MUYPUisNsbfH0b1IfmCygg'
