c3a7b7f0911a36807f3ac16ff6dd9b74c8e62dad
shensheng <ss707494@163.com>
2020-09-15 15:56:57 +0800

    fix bug

--- src/views/PromoCode/PromoCode/PromoCode.tsx
+++ src/views/PromoCode/PromoCode/PromoCode.tsx
@@ -1,18 +1,20 @@
-import React, {useEffect} from "react"
-import {CusButton} from "@/component/CusButton"
-import {S} from "@/views/Order/List/style"
-import CircularProgress from "@material-ui/core/CircularProgress"
-import TableHead from "@material-ui/core/TableHead"
-import {TableBody, TableRow} from "@material-ui/core"
-import {CusTableCell as TableCell} from "@/component/CusTableCell"
-import {StyleTableBox} from "@/common/style/tableBox"
-import {useMutationSimpleData, useQuerySimpleData} from "@/component/ApolloQuery"
-import {promo_code_list, save_promo_code} from "@/views/PromoCode/graphql"
-import {EditModal} from "@/views/PromoCode/EditModal/EditModal"
-import {useCommonModalState} from "@/common/useHooks"
-import {formatDate} from "@/common/utils"
-import styled from "styled-components"
-import {DiscountConditionEnum, DiscountTypeEnum, PromoCodeTypeEnum} from "@/common/ss_common/enum"
+import React, {useEffect} from 'react'
+import {CusButton} from '@/component/CusButton'
+import {S} from '@/views/Order/List/style'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import TableHead from '@material-ui/core/TableHead'
+import {TableBody, TableRow} from '@material-ui/core'
+import {CusTableCell as TableCell} from '@/component/CusTableCell'
+import {StyleTableBox} from '@/common/style/tableBox'
+import {useMutationSimpleData, useQuerySimpleData} from '@/component/ApolloQuery'
+import {promo_code_list, save_promo_code} from '@/views/PromoCode/graphql'
+import {EditModal} from '@/views/PromoCode/EditModal/EditModal'
+import {useCommonModalState} from '@/common/useHooks'
+import {formatDate} from '@/common/utils'
+import styled from 'styled-components'
+import {DiscountConditionEnum, DiscountTypeEnum, PromoCodeTypeEnum} from '@/common/ss_common/enum'
+import {useStoreModelByType__Graphql} from '@/common/ModelAction/useStore'
+import {dictAllListModel} from '@/views/Dictionary/dictAllListModel'
 
 const ImgBox = styled.img`
   max-width: 90px;
@@ -20,14 +22,19 @@ const ImgBox = styled.img`
 `
 
 export const PromoCodeTable = ({promoCodeType, theme}: any) => {
+  const {actions: dictAllListAction, state: dictAllListState} = useStoreModelByType__Graphql(dictAllListModel)
+  useEffect(() => {
+    dictAllListAction.getDictList()
+  }, [dictAllListAction])
+
   const commonModalState: CommonModalState = useCommonModalState()
   const [getPromoCodeList, {promo_code_list: promoCodeList}, listLoad] = useQuerySimpleData(promo_code_list)
-  const [savePromoCode, , ] = useMutationSimpleData(save_promo_code)
+  const [savePromoCode] = useMutationSimpleData(save_promo_code)
   const tableHeader = [
-    '名称', '优惠码', '折扣类型', '使用条件', '使用类别', '展示图片', '有效时间-开始', '有效时间-结束', '描述'
+    '名称', '优惠码', '折扣类型', '使用条件', '使用类别', '展示图片', '有效时间-开始', '有效时间-结束', '描述',
   ]
   const tableHeaderDarenCard = [
-    '名称', '重复次数', '折扣类型', '使用条件', '使用类别', '展示图片', '有效时间-开始', '有效时间-结束', '描述'
+    '名称', '优惠码', '用户等级', '重复次数', '折扣类型', '使用条件', '使用类别', '展示图片', '有效时间-开始', '有效时间-结束', '描述',
   ]
   const dealTableHeader: any = ({
     [PromoCodeTypeEnum.PromoCode]: tableHeader,
@@ -45,8 +52,8 @@ export const PromoCodeTable = ({promoCodeType, theme}: any) => {
       <div>
         <header>
           <CusButton
-              variant={"contained"}
-              color={"primary"}
+              variant={'contained'}
+              color={'primary'}
               onClick={commonModalState.openClick({})}
           >新增</CusButton>
         </header>
@@ -67,8 +74,18 @@ export const PromoCodeTable = ({promoCodeType, theme}: any) => {
                   {promoCodeList?.map((e: PromoCode) => <TableRow key={`promoCodeList${e?.id}`}>
                     <TableCell>{e?.title}</TableCell>
                     <TableCell>
-                      {promoCodeType === PromoCodeTypeEnum.PromoCode ? e?.code : e?.reuse_times}
+                      {e?.code}
                     </TableCell>
+                    {promoCodeType === PromoCodeTypeEnum.DarenCard &&
+                    <>
+                      <TableCell>
+                        {dictAllListState.userLevelList?.find(value => value.id === e?.userLevelId)?.name}
+                      </TableCell>
+                      <TableCell>
+                        {e?.reuse_times}
+                      </TableCell>
+                    </>
+                    }
                     <TableCell>
                       {`${e?.discount_type === DiscountTypeEnum.Percentage ? '百分比' : '金额'}折扣,减${e?.discount_amount}${e?.discount_type === DiscountTypeEnum.Percentage ? '%' : '元'}`}
                     </TableCell>
@@ -85,12 +102,12 @@ export const PromoCodeTable = ({promoCodeType, theme}: any) => {
                     <TableCell>
                       <CusButton
                           style={{marginRight: '10px'}}
-                          variant={"outlined"}
+                          variant={'outlined'}
                           onClick={commonModalState.openClick({...e})}
                       >编辑</CusButton>
                       <CusButton
                           color={e.isDisable ? 'secondary' : 'primary'}
-                          variant={"outlined"}
+                          variant={'outlined'}
                           onClick={async () => {
                             await savePromoCode({
                               id: e?.id,
